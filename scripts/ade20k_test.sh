d0d5ec18953b173acb54f6c6ca989f6cdc7a6d8a
emily-lin <emily.lin@berkeley.edu>
2023-08-21 06:25:19 +0000

    Add working batch visualization and verified ADE20K evaluation (mIOU=44.41).

---
+++ scripts/ade20k_test.sh
@@ -0,0 +1,1 @@
+python tools/test.py configs/swin/swin-tiny-patch4-window7-in1k-pre_upernet_8xb2-160k_ade20k-512x512.py data/checkpoints/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k_pretrain_224x224_1K_20210531_112542-e380ad3e.pth
