7eb5ef463c37b0616b7516b656bf22f35d0575e9
emily-lin <emily.lin@berkeley.edu>
2023-08-24 06:08:56 +0000

    Update to use 128K best checkpoint

--- scripts/track_test_tta.sh
+++ scripts/track_test_tta.sh
@@ -1,1 +1,1 @@
-python tools/test.py configs/swin/swin-tiny-track-512x512.py ./work_dirs/20220627_i160k/latest.pth --tta
+python tools/test.py configs/swin/swin-tiny-track-512x512.py ./work_dirs/20220627_i160k/best_mDice_iter_128000.pth --tta
