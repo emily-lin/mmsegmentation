2b92fe77f9ce0c6ad452d029910eb437cfdc2a52
emily-lin <emily.lin@berkeley.edu>
2023-08-25 05:23:00 +0000

    Generate tta script

--- scripts/track_test_tta.sh
+++ scripts/track_test_tta.sh
@@ -1,1 +1,2 @@
 python tools/test.py configs/swin/swin-tiny-track-512x512.py ./work_dirs/20220627_i160k/best_mDice_iter_128000.pth --tta
+#python tools/test.py configs/swin/swin-tiny-track-512x512.py ./work_dirs/20220627_i160k/best_mDice_iter_128000.pth --tta --show-dir=/tmp/0824_tta_128k_full_vis
