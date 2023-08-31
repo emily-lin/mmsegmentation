9a1a68d7171de4066286164d0b5ceb3d1e2208cb
Peng Lu <penglu2097@gmail.com>
2023-08-31 12:02:19 +0800

    [Feature] Support depth metrics (#3297)
    Thanks for your contribution and we appreciate it a lot. The following
    instructions would make your pull request more healthy and more easily
    get feedback. If you do not understand some items, don't worry, just
    make the pull request and seek help from maintainers.
    
    Please describe the motivation of this PR and the goal you want to
    achieve through this PR.
    
    Support metrics for the depth estimation task, including RMSE, ABSRel,
    and etc.
    
    Please briefly describe what modification is made in this PR.
    
    Does the modification introduce changes that break the
    backward-compatibility of the downstream repos?
    If so, please describe how it breaks the compatibility and how the
    downstream projects should modify their code to keep compatibility with
    this PR.
    
    Using the following configuration to compute depth metrics on NYU
    
    ```python
    dataset_type = 'NYUDataset'
    data_root = 'data/nyu'
    
    test_pipeline = [
        dict(type='LoadImageFromFile'),
        dict(dict(type='LoadDepthAnnotation', depth_rescale_factor=1e-3)),
        dict(
            type='PackSegInputs',
            meta_keys=('img_path', 'depth_map_path', 'ori_shape', 'img_shape',
                       'pad_shape', 'scale_factor', 'flip', 'flip_direction',
                       'category_id'))
    ]
    
    val_dataloader = dict(
        batch_size=1,
        num_workers=4,
        persistent_workers=True,
        sampler=dict(type='DefaultSampler', shuffle=False),
        dataset=dict(
            type=dataset_type,
            data_root=data_root,
            test_mode=True,
            data_prefix=dict(
                img_path='images/test', depth_map_path='annotations/test'),
            pipeline=test_pipeline))
    test_dataloader = val_dataloader
    
    val_evaluator = dict(type='DepthMetric', max_depth_eval=10.0, crop_type='nyu')
    test_evaluator = val_evaluator
    ```
    
    Example log:
    
    ![image](https://github.com/open-mmlab/mmsegmentation/assets/26127467/8101d65c-dee6-48de-916c-818659947b59)
    
    1. Pre-commit or other linting tools are used to fix the potential lint
    issues.
    2. The modification is covered by complete unit tests. If not, please
    add more unit test to ensure the correctness.
    3. If the modification has potential influence on downstream projects,
    this PR should be tested with downstream projects, like MMDet or
    MMDet3D.
    4. The documentation has been modified accordingly, like docstring or
    example tutorials.

--- mmseg/evaluation/metrics/__init__.py
+++ mmseg/evaluation/metrics/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .citys_metric import CityscapesMetric
+from .depth_metric import DepthMetric
 from .iou_metric import IoUMetric
 from .iou_metric import IoUROCMetric
 
-__all__ = ['IoUMetric', 'CityscapesMetric', 'IoUROCMetric']
+__all__ = ['IoUMetric', 'CityscapesMetric', 'IoUROCMetric', 'DepthMetric']
