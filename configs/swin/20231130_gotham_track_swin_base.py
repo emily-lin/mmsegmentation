0bc19c503427492396ee751758493065bad5bf3d
emily-lin <emily.lin@berkeley.edu>
2023-12-13 19:03:58 +0000

    Hyperparameter tuning: batch 60, crop 160, lr change, remove ohem

--- configs/swin/20231130_gotham_track_swin_base.py
+++ configs/swin/20231130_gotham_track_swin_base.py
@@ -8,15 +8,14 @@ model = dict(
         embed_dims=128,
         depths=[2, 2, 18, 2],
         num_heads=[4, 8, 16, 32]),
-    decode_head=dict(in_channels=[128, 256, 512, 1024],
-        sampler = dict(type = 'OHEMPixelSampler', thresh = 0.7, min_kept = 100000)),
+    decode_head=dict(in_channels=[128, 256, 512, 1024]),
     auxiliary_head=dict(in_channels=512))
 
 # AdamW optimizer, no weight decay for position embedding & layer norm in backbone
 optim_wrapper = dict(
         type='OptimWrapper',
         optimizer = dict(
-            type = 'AdamW', lr = 6E-5, betas = (0.9, 0.999), weight_decay = 0.01),
+            type = 'AdamW', lr = 1.8E-4, betas = (0.9, 0.999), weight_decay = 0.01),
         paramwise_cfg = dict(
             custom_keys = {
                 'absolute_pos_embed': dict(decay_mult = 0.),
@@ -24,3 +23,5 @@ optim_wrapper = dict(
                 'norm': dict(decay_mult = 0.),
                 'head': dict(lr_mult = 5.)
             }))
+
+train_dataloader = dict(batch_size = 60)
