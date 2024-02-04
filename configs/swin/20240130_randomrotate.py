6ba3e3fc5e6d0ccee0c861efe262ccc364c62215
emily-lin <emily.lin@berkeley.edu>
2024-02-04 23:15:05 +0000

    Config file for 20240130_randomrotate.py. Code state for Row 67 of MMSeg Restarted (NEW).

---
+++ configs/swin/20240130_randomrotate.py
@@ -0,0 +1,27 @@
+_base_ = [
+    './20231118_gotham_track_swin_tiny.py'
+]
+checkpoint_file = 'pretrain/upernet_swinB_pretrain_ImageNet22K_ade20k_row4.pth'  # noqa
+model = dict(
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file),
+        embed_dims=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32]),
+    decode_head=dict(in_channels=[128, 256, 512, 1024]),
+    auxiliary_head=dict(in_channels=512))
+
+# AdamW optimizer, no weight decay for position embedding & layer norm in backbone
+optim_wrapper = dict(
+        type='OptimWrapper',
+        optimizer = dict(
+            type = 'AdamW', lr = 1.8E-4, betas = (0.9, 0.999), weight_decay = 0.01),
+        paramwise_cfg = dict(
+            custom_keys = {
+                'absolute_pos_embed': dict(decay_mult = 0.),
+                'relative_position_bias_table': dict(decay_mult = 0.),
+                'norm': dict(decay_mult = 0.),
+                'head': dict(lr_mult = 5.)
+            }))
+
+train_dataloader = dict(batch_size = 60)
